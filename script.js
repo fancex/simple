e3ceaf70a6911de5a4b40e4c4f964572e1d27941
fancex <lucaf90@hotmail.it>
2013-10-07 21:48:59 +0200

    menu and navbar

--- script.js
+++ script.js
@@ -26,8 +26,10 @@ $('.simpletab-ul li').click(function(){
 });
 /*simple tip */
 $( ".simple-tipmenu" ).each(function( index ) {
+	/*add close button at top of every tip*/
 	item=$(this);
 	item.html('<div class="simple-tip" target="'+item.attr('id')+'" style="float:right;cursor:pointer;" title="chiudi">X</div><div style="clear:both;border-bottom:solid grey 1px;"></div>'+item.html());
+	/*end add...*/
 	$('#'+li.attr('target')).show();
 });
 
@@ -40,5 +42,37 @@ $('.simple-tip').click(function(){
 		target.css('left',$(this).position().left+$(this).width());
 		$(target).show();
 	}
+
+});
+
+
+/*simple menu*/
+$('.simple-menu li a').click(function(){
+	ul=$(this).parent('li');
+	child=ul.children('ul');
+	child.slideToggle();
+});
+
+/*simple navbar*/
+$('.drop-menu').hide();
+$('.simple-bar').mouseleave(function(){
+	//$('.drop-menu').slideUp();
+});
+$('.simple-bar-nav li a').hover(function(){
+	
+});
+$('.bar-drop').click(function(){
+	genitore=$(this).parent('.bar-dropdown');
+	target=$(genitore).find('.drop-menu');
+	if(target.is(':visible')===false){
+		pos=$(genitore).position();
+		target.css('left',pos.left-0);
+		target.css('top',$('#bottombar').position().top-2);
+		$('.drop-menu').hide();
+		target.slideToggle();
+	}else{
+		target.slideUp();
+	}
+	
 
 });
